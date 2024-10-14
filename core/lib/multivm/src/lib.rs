wuzhanfly/zksync-era

commit 93bc66f21f9f67a440f06f1c4402e0d687698741

feat(vm): Split old and new VM implementations (#2915)
## What ❔

Splits old and new VM implementations in the `multivm` crate:

- Old VMs are encapsulated in the `LegacyVmInstance` enum, while new
ones in the `FastVmInstance` enum (which includes plain and shadowed VM
variants).
- Fast VM and `FastVmInstance` now expose a tracer type.
- Usage of the Fast VM in the batch executor are updated
correspondingly.

## Why ❔

It seems infeasible to unify the tracer model for old and new VMs, so
keeping them all in a single enum makes little sense.

## Checklist

- [x] PR title corresponds to the body of PR (we generate changelog
entries from PRs).
- [x] Tests for the changes have been added / updated.
- [x] Documentation comments have been added / updated.
- [x] Code has been formatted via `zk fmt` and `zk lint`.

core/lib/multivm/src/lib.rs
@@ -16,7 +16,7 @@ pub use crate::{
         vm_1_3_2, vm_1_4_1, vm_1_4_2, vm_boojum_integration, vm_fast, vm_latest, vm_m5, vm_m6,
         vm_refunds_enhancement, vm_virtual_blocks,
     },
-    vm_instance::VmInstance,
+    vm_instance::{FastVmInstance, LegacyVmInstance},
 };
 
 mod glue;

core/lib/multivm/src/versions/vm_fast/vm.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt};
+use std::{collections::HashMap, fmt, mem};
 
 use zk_evm_1_5_0::zkevm_opcode_defs::system_params::INITIAL_FRAME_FORMAL_EH_LOCATION;
 use zksync_contracts::SystemContractCode;
@@ -56,9 +56,16 @@ use crate::{
 
 const VM_VERSION: MultiVMSubversion = MultiVMSubversion::IncreasedBootloaderMemory;
 
-pub struct Vm<S> {
-    pub(crate) world: World<S, CircuitsTracer>,
-    pub(crate) inner: VirtualMachine<CircuitsTracer, World<S, CircuitsTracer>>,
+type FullTracer<Tr> = (Tr, CircuitsTracer);
+
+/// Fast VM wrapper.
+///
+/// The wrapper is parametric by the storage and tracer types. Besides the [`Tracer`] trait, a tracer must have `'static` lifetime
+/// and implement [`Default`] (the latter is necessary to complete batches). [`CircuitsTracer`] is currently always enabled;
+/// you don't need to specify it explicitly.
+pub struct Vm<S, Tr = ()> {
+    pub(crate) world: World<S, FullTracer<Tr>>,
+    pub(crate) inner: VirtualMachine<FullTracer<Tr>, World<S, FullTracer<Tr>>>,
     gas_for_account_validation: u32,
     pub(crate) bootloader_state: BootloaderState,
     pub(crate) batch_env: L1BatchEnv,
@@ -68,7 +75,7 @@ pub struct Vm<S> {
     enforced_state_diffs: Option<Vec<StateDiffRecord>>,
 }
 
-impl<S: ReadStorage> Vm<S> {
+impl<S: ReadStorage, Tr: Tracer> Vm<S, Tr> {
     pub fn custom(batch_env: L1BatchEnv, system_env: SystemEnv, storage: S) -> Self {
         let default_aa_code_hash = system_env
             .base_system_smart_contracts
@@ -131,7 +138,7 @@ impl<S: ReadStorage> Vm<S> {
     fn run(
         &mut self,
         execution_mode: VmExecutionMode,
-        tracer: &mut CircuitsTracer,
+        tracer: &mut (Tr, CircuitsTracer),
         track_refunds: bool,
     ) -> (ExecutionResult, Refunds) {
         let mut refunds = Refunds {
@@ -485,7 +492,11 @@ impl<S: ReadStorage> Vm<S> {
     }
 }
 
-impl<S: ReadStorage> VmFactory<StorageView<S>> for Vm<ImmutableStorageView<S>> {
+impl<S, Tr> VmFactory<StorageView<S>> for Vm<ImmutableStorageView<S>, Tr>
+where
+    S: ReadStorage,
+    Tr: Tracer + Default + 'static,
+{
     fn new(
         batch_env: L1BatchEnv,
         system_env: SystemEnv,
@@ -496,8 +507,8 @@ impl<S: ReadStorage> VmFactory<StorageView<S>> for Vm<ImmutableStorageView<S>> {
     }
 }
 
-impl<S: ReadStorage> VmInterface for Vm<S> {
-    type TracerDispatcher = ();
+impl<S: ReadStorage, Tr: Tracer + Default + 'static> VmInterface for Vm<S, Tr> {
+    type TracerDispatcher = Tr;
 
     fn push_transaction(&mut self, tx: zksync_types::Transaction) {
         self.push_transaction_inner(tx, 0, true);
@@ -505,7 +516,7 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
 
     fn inspect(
         &mut self,
-        (): Self::TracerDispatcher,
+        tracer: &mut Self::TracerDispatcher,
         execution_mode: VmExecutionMode,
     ) -> VmExecutionResultAndLogs {
         let mut track_refunds = false;
@@ -515,12 +526,14 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
             track_refunds = true;
         }
 
-        let mut tracer = CircuitsTracer::default();
         let start = self.inner.world_diff().snapshot();
         let pubdata_before = self.inner.pubdata();
         let gas_before = self.gas_remaining();
 
-        let (result, refunds) = self.run(execution_mode, &mut tracer, track_refunds);
+        let mut full_tracer = (mem::take(tracer), CircuitsTracer::default());
+        let (result, refunds) = self.run(execution_mode, &mut full_tracer, track_refunds);
+        *tracer = full_tracer.0; // place the tracer back
+
         let ignore_world_diff = matches!(execution_mode, VmExecutionMode::OneTx)
             && matches!(result, ExecutionResult::Halt { .. });
 
@@ -572,7 +585,6 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
         };
 
         let pubdata_after = self.inner.pubdata();
-        let circuit_statistic = tracer.circuit_statistic();
         let gas_remaining = self.gas_remaining();
         VmExecutionResultAndLogs {
             result,
@@ -586,7 +598,7 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
                 computational_gas_used: 0,
                 total_log_queries: 0,
                 pubdata_published: (pubdata_after - pubdata_before).max(0) as u32,
-                circuit_statistic,
+                circuit_statistic: full_tracer.1.circuit_statistic(),
             },
             refunds,
         }
@@ -594,12 +606,12 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
 
     fn inspect_transaction_with_bytecode_compression(
         &mut self,
-        (): Self::TracerDispatcher,
+        tracer: &mut Self::TracerDispatcher,
         tx: zksync_types::Transaction,
         with_compression: bool,
     ) -> (BytecodeCompressionResult<'_>, VmExecutionResultAndLogs) {
         self.push_transaction_inner(tx, 0, with_compression);
-        let result = self.inspect((), VmExecutionMode::OneTx);
+        let result = self.inspect(tracer, VmExecutionMode::OneTx);
 
         let compression_result = if self.has_unpublished_bytecodes() {
             Err(BytecodeCompressionError::BytecodeCompressionFailed)
@@ -621,7 +633,7 @@ impl<S: ReadStorage> VmInterface for Vm<S> {
     }
 
     fn finish_batch(&mut self) -> FinishedL1Batch {
-        let result = self.inspect((), VmExecutionMode::Batch);
+        let result = self.inspect(&mut Tr::default(), VmExecutionMode::Batch);
         let execution_state = self.get_current_execution_state();
         let bootloader_memory = self.bootloader_state.bootloader_memory();
         FinishedL1Batch {
@@ -650,7 +662,7 @@ struct VmSnapshot {
     gas_for_account_validation: u32,
 }
 
-impl<S: ReadStorage> VmInterfaceHistoryEnabled for Vm<S> {
+impl<S: ReadStorage, Tr: Tracer + Default + 'static> VmInterfaceHistoryEnabled for Vm<S, Tr> {
     fn make_snapshot(&mut self) {
         assert!(
             self.snapshot.is_none(),
@@ -687,7 +699,7 @@ impl<S: ReadStorage> VmTrackingContracts for Vm<S> {
     }
 }
 
-impl<S: fmt::Debug> fmt::Debug for Vm<S> {
+impl<S: fmt::Debug, Tr: fmt::Debug> fmt::Debug for Vm<S, Tr> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Vm")
             .field(

commit 66e44105c86a0450d8fccc417dc48ac404118df7

fix(vm): Check protocol version for fast VM (#3080)
## What ❔

Fixes a regression introduced by
https://github.com/matter-labs/zksync-era/pull/2915: The protocol
version is now not checked before instantiating a fast VM.

## Why ❔

Without this check, fast VM can be used with old protocol versions,
which most likely will cause divergencies.

## Checklist

- [x] PR title corresponds to the body of PR (we generate changelog
entries from PRs).
- [x] Tests for the changes have been added / updated.
- [x] Documentation comments have been added / updated.
- [x] Code has been formatted via `zk_supervisor fmt` and `zk_supervisor
lint`.

core/lib/multivm/src/lib.rs
@@ -16,7 +16,7 @@ pub use crate::{
         vm_1_3_2, vm_1_4_1, vm_1_4_2, vm_boojum_integration, vm_fast, vm_latest, vm_m5, vm_m6,
         vm_refunds_enhancement, vm_virtual_blocks,
     },
-    vm_instance::{FastVmInstance, LegacyVmInstance},
+    vm_instance::{is_supported_by_fast_vm, FastVmInstance, LegacyVmInstance},
 };
 
 mod glue;

core/lib/multivm/src/versions/vm_fast/vm.rs
@@ -40,6 +40,7 @@ use crate::{
         VmExecutionStatistics, VmFactory, VmInterface, VmInterfaceHistoryEnabled, VmRevertReason,
         VmTrackingContracts,
     },
+    is_supported_by_fast_vm,
     utils::events::extract_l2tol1logs_from_l1_messenger,
     vm_fast::{
         bootloader_state::utils::{apply_l2_block, apply_pubdata_to_memory},
@@ -104,6 +105,12 @@ pub struct Vm<S, Tr = ()> {
 
 impl<S: ReadStorage, Tr: Tracer> Vm<S, Tr> {
     pub fn custom(batch_env: L1BatchEnv, system_env: SystemEnv, storage: S) -> Self {
+        assert!(
+            is_supported_by_fast_vm(system_env.version),
+            "Protocol version {:?} is not supported by fast VM",
+            system_env.version
+        );
+
         let default_aa_code_hash = system_env
             .base_system_smart_contracts
             .default_aa

core/lib/multivm/src/vm_instance.rs
@@ -1,6 +1,6 @@
 use std::mem;
 
-use zksync_types::{vm::VmVersion, Transaction};
+use zksync_types::{vm::VmVersion, ProtocolVersionId, Transaction};
 use zksync_vm2::interface::Tracer;
 
 use crate::{
@@ -327,4 +327,12 @@ impl<S: ReadStorage, Tr: Tracer + Default + 'static> FastVmInstance<S, Tr> {
     ) -> Self {
         Self::Shadowed(ShadowedFastVm::new(l1_batch_env, system_env, storage_view))
     }
+}
+
+/// Checks whether the protocol version is supported by the fast VM.
+pub fn is_supported_by_fast_vm(protocol_version: ProtocolVersionId) -> bool {
+    matches!(
+        protocol_version.into(),
+        VmVersion::Vm1_5_0IncreasedBootloaderMemory
+    )
 }

core/lib/vm_executor/Cargo.toml
@@ -23,3 +23,6 @@ tokio.workspace = true
 anyhow.workspace = true
 tracing.workspace = true
 vise.workspace = true
+
+[dev-dependencies]
+assert_matches.workspace = true

core/lib/vm_executor/src/batch/factory.rs
@@ -12,6 +12,7 @@ use zksync_multivm::{
         ExecutionResult, FinishedL1Batch, Halt, L1BatchEnv, L2BlockEnv, SystemEnv, VmFactory,
         VmInterface, VmInterfaceHistoryEnabled,
     },
+    is_supported_by_fast_vm,
     tracers::CallTracer,
     vm_fast,
     vm_latest::HistoryEnabled,
@@ -159,6 +160,10 @@ impl<S: ReadStorage, Tr: BatchTracer> BatchVm<S, Tr> {
         storage_ptr: StoragePtr<StorageView<S>>,
         mode: FastVmMode,
     ) -> Self {
+        if !is_supported_by_fast_vm(system_env.version) {
+            return Self::Legacy(LegacyVmInstance::new(l1_batch_env, system_env, storage_ptr));
+        }
+
         match mode {
             FastVmMode::Old => {
                 Self::Legacy(LegacyVmInstance::new(l1_batch_env, system_env, storage_ptr))
@@ -441,5 +446,52 @@ impl<S: ReadStorage + 'static, Tr: BatchTracer> CommandReceiver<S, Tr> {
                 call_traces: vec![],
             })
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+    use zksync_multivm::interface::{storage::InMemoryStorage, TxExecutionMode};
+    use zksync_types::ProtocolVersionId;
+
+    use super::*;
+    use crate::testonly::{default_l1_batch_env, default_system_env, FAST_VM_MODES};
+
+    #[test]
+    fn selecting_vm_for_execution() {
+        let l1_batch_env = default_l1_batch_env(1);
+        let mut system_env = SystemEnv {
+            version: ProtocolVersionId::Version22,
+            ..default_system_env(TxExecutionMode::VerifyExecute)
+        };
+        let storage = StorageView::new(InMemoryStorage::default()).to_rc_ptr();
+        for mode in FAST_VM_MODES {
+            let vm = BatchVm::<_, ()>::new(
+                l1_batch_env.clone(),
+                system_env.clone(),
+                storage.clone(),
+                mode,
+            );
+            assert_matches!(vm, BatchVm::Legacy(_));
+        }
+
+        system_env.version = ProtocolVersionId::latest();
+        let vm = BatchVm::<_, ()>::new(
+            l1_batch_env.clone(),
+            system_env.clone(),
+            storage.clone(),
+            FastVmMode::Old,
+        );
+        assert_matches!(vm, BatchVm::Legacy(_));
+        let vm = BatchVm::<_, ()>::new(
+            l1_batch_env.clone(),
+            system_env.clone(),
+            storage.clone(),
+            FastVmMode::New,
+        );
+        assert_matches!(vm, BatchVm::Fast(FastVmInstance::Fast(_)));
+        let vm = BatchVm::<_, ()>::new(l1_batch_env, system_env, storage, FastVmMode::Shadow);
+        assert_matches!(vm, BatchVm::Fast(FastVmInstance::Shadowed(_)));
     }
 }

core/lib/vm_executor/src/lib.rs
@@ -9,3 +9,5 @@ pub mod batch;
 pub mod oneshot;
 mod shared;
 pub mod storage;
+#[cfg(test)]
+mod testonly;

core/lib/vm_executor/src/testonly.rs
@@ -1,0 +1,45 @@
+use once_cell::sync::Lazy;
+use zksync_contracts::BaseSystemContracts;
+use zksync_multivm::{
+    interface::{L1BatchEnv, L2BlockEnv, SystemEnv, TxExecutionMode},
+    vm_latest::constants::BATCH_COMPUTATIONAL_GAS_LIMIT,
+};
+use zksync_types::{
+    block::L2BlockHasher, fee_model::BatchFeeInput, vm::FastVmMode, Address, L1BatchNumber,
+    L2BlockNumber, L2ChainId, ProtocolVersionId, H256, ZKPORTER_IS_AVAILABLE,
+};
+
+static BASE_SYSTEM_CONTRACTS: Lazy<BaseSystemContracts> =
+    Lazy::new(BaseSystemContracts::load_from_disk);
+
+pub(crate) const FAST_VM_MODES: [FastVmMode; 3] =
+    [FastVmMode::Old, FastVmMode::New, FastVmMode::Shadow];
+
+pub(crate) fn default_system_env(execution_mode: TxExecutionMode) -> SystemEnv {
+    SystemEnv {
+        zk_porter_available: ZKPORTER_IS_AVAILABLE,
+        version: ProtocolVersionId::latest(),
+        base_system_smart_contracts: BASE_SYSTEM_CONTRACTS.clone(),
+        bootloader_gas_limit: BATCH_COMPUTATIONAL_GAS_LIMIT,
+        execution_mode,
+        default_validation_computational_gas_limit: BATCH_COMPUTATIONAL_GAS_LIMIT,
+        chain_id: L2ChainId::default(),
+    }
+}
+
+pub(crate) fn default_l1_batch_env(number: u32) -> L1BatchEnv {
+    L1BatchEnv {
+        previous_batch_hash: Some(H256::zero()),
+        number: L1BatchNumber(number),
+        timestamp: number.into(),
+        fee_account: Address::repeat_byte(0x22),
+        enforced_base_fee: None,
+        first_l2_block: L2BlockEnv {
+            number,
+            timestamp: number.into(),
+            prev_block_hash: L2BlockHasher::legacy_hash(L2BlockNumber(number - 1)),
+            max_virtual_blocks_to_create: 1,
+        },
+        fee_input: BatchFeeInput::sensible_l1_pegged_default(),
+    }
+}